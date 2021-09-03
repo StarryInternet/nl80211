8868a2f56c400c47077c25a442ff3ec61878af18
Property404 <dmartinez@starry.com>
2021-09-03 10:23:25 -0400

    Don't store null characters after strings
    Nl80211 was exposing C style ends-with-null-byte -style strings.
    
    This caused an issue when attempting to compare with a string constant
    in Rust.

--- src/parse_attr.rs
+++ src/parse_attr.rs
@@ -3,7 +3,9 @@ use std::convert::TryInto;
 
 /// Parse a vec of bytes as a String
 pub fn parse_string(input: &[u8]) -> String {
-    String::from_utf8_lossy(input).to_owned().to_string()
+    String::from_utf8_lossy(input)
+        .trim_matches(char::from(0))
+        .to_string()
 }
 
 /// Parse a vec of bytes as a mac address
@@ -72,6 +74,12 @@ mod test_type_conversion {
         let input_string = "test".to_string();
         let bytes_string = input_string.as_bytes().to_vec();
         assert_eq!(parse_string(&bytes_string), input_string);
+    }
+
+    #[test]
+    fn test_parse_string_trim_zeros() {
+        let input = [0x48, 0x45, 0x4C, 0x4C, 0x4F, 0x00];
+        assert_eq!(parse_string(&input), "HELLO");
     }
 
     #[test]
